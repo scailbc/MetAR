3c051ad09b3985320e602fd9bf3dca38ddef8346
scailbc <s208912studenti.polito.it>
2017-10-21 21:45:13 +0200

    First Commit - Empty Activity

---
+++ app/src/main/java/it/drakefk/metar/MetARMain.java
@@ -0,0 +1,13 @@
+package it.drakefk.metar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MetARMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.metar_main);
+    }
+}
